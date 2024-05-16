47eede717186f5f5bd43877ab8635f608bd0c7e5
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-05-16 12:23:10 +0700

    finished ex 1-6

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,12 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
+for (let score of scores) {
+  if (minScore === undefined) {
+    minScore = score;
+  } else if (score < minScore) {
+    minScore = score;
+  }
+}
 
 console.log(minScore);
--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,10 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+for (let restaurant of restaurants) {
+  let newName = "";
+  newName = "Restaurant Name: " + restaurant;
+  newRestaurants.push(newName);
+}
 
 console.log(newRestaurants);
