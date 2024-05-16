8f5cb2e3adb2b1bdb72333e28dd74cb54ce44a6c
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-05-16 13:14:57 +0700

    feat:done all exercises

--- ex-3.js
+++ ex-3.js
@@ -3,4 +3,10 @@ let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
 
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] < scores[i + 1]) {
+    minScore = scores[i];
+  }
+}
+
 console.log(minScore);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,8 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+for (let i = 1; i <= companyName.length; i++) {
+  reversedCompanyName += companyName[companyName.length - i];
+}
 
 console.log(reversedCompanyName);
